76a90268082a7e75096b681009fae6cd9a5ef8ce
Ilja Korsun <ilja.korsun@gmail.com>
2014-10-09 03:07:53 +0200

    more optimized initializers

--- Program.cs
+++ Program.cs
@@ -17,36 +17,40 @@ namespace LoLUpdater
 .Cast<ManagementBaseObject>()
 .Sum(item => ToInt(item["NumberOfCores"].ToString())) > 1;
 
-        // Some of these processors are not even out, included them to skip to have to do future updates.
-        private static readonly string[] Cpulist = { "Haswell", "Broadwell", "Skylake", "Cannonlake" };
-
-        private static readonly bool IsAvx2 = new ManagementObjectSearcher("Select * from Win32_Processor").Get()
-        .Cast<ManagementBaseObject>()
-        .Any(item => item["Name"].ToString().Contains(Cpulist.ToString()));
-
         private static int _userInput;
+
         private static readonly bool IsInstalling = _userInput == 1;
         private static bool _notdone;
-        private static readonly bool HasSse2 = IsProcessorFeaturePresent(10);
-        private static readonly bool HasSse = IsProcessorFeaturePresent(6);
-        private static readonly bool IsUnix = (int)Environment.OSVersion.Platform == 6;
-        private static readonly bool IsAtLeastWinNt6 = Environment.OSVersion.Version.Major >= 6;
+        private static Uri _tempUri;
+        private static readonly bool IsNotGarena = Directory.Exists("RADS");
         private static readonly bool Isx64 = Environment.Is64BitProcess;
+        private static readonly bool AvxCheck = Isx64 && (Environment.OSVersion.Version.Major >= 6 || (int)Environment.OSVersion.Platform == 6);
+        private static readonly bool HasSse = IsProcessorFeaturePresent(6);
+        private static readonly bool HasSse2 = IsProcessorFeaturePresent(10);
+        private static readonly bool HasAvx = AvxCheck && IsProcessorFeaturePresent(17);
+
+        private static readonly bool IsAvx2 = AvxCheck && new ManagementObjectSearcher("Select * from Win32_Processor").Get()
+.Cast<ManagementBaseObject>()
+.Any(item => item["Name"].ToString().Contains(new[] { "Haswell", "Broadwell", "Skylake", "Cannonlake" }.ToString()));
+
         private static readonly string SlnFolder = Version("solutions", "lol_game_client_sln");
         private static readonly string AirFolder = Version("projects", "lol_air_client");
         private static readonly string[] LoLProccess = { "LoLClient", "LoLLauncher", "LoLPatcher", "League of Legends", "PMB" };
-        private const string Tweak = "DefaultParticleMultiThreading=1";
 
-        private static Uri _tempUri;
+        // Not sure if this tweak is designed for multi-core or not Currently this patch removes
+        // this tweak from single core systems, I dont know what effect this has.
+        private const string CfgTweak = "DefaultParticleMultiThreading=1";
 
-        private static readonly Uri TbbUri = new Uri((Uri.TryCreate(new Uri("https://github.com/Loggan08/LoLUpdater/raw/master/Resources/"),
-            IsMultiCore
-                ? (Isx64 && (IsAtLeastWinNt6 || IsUnix) && IsAvx2
-                    ? "Avx2.dll"
-                    : (Isx64 && (IsAtLeastWinNt6 || IsUnix) && IsProcessorFeaturePresent(17)
-                        ? "Avx.dll"
-                        : (HasSse2 ? "Sse2.dll" : HasSse ? "Sse.dll" : "Tbb.dll")))
-                : (HasSse2 ? "Sse2St.dll" : (HasSse ? "SseSt.dll" : "TbbSt.dll")), out _tempUri) ? _tempUri : new Uri(String.Empty)).ToString());
+        private static readonly Uri TbbUri =
+            new Uri(((Uri.TryCreate(new Uri("https://github.com/Loggan08/LoLUpdater/raw/master/Resources/"),
+                IsMultiCore
+                    ? (IsAvx2
+                        ? "Avx2.dll"
+                        : (HasAvx
+                            ? "Avx.dll"
+                            : (HasSse2 ? "Sse2.dll" : HasSse ? "Sse.dll" : "Tbb.dll")))
+                    : (HasSse2 ? "SseSt.dll" : (HasSse ? "SseSt.dll" : "TbbSt.dll")), out _tempUri))
+                ? _tempUri : new Uri(string.Empty)).ToString());
 
         private static readonly Uri FlashUri =
             new Uri("https://github.com/Loggan08/LoLUpdater/raw/master/Resources/NPSWF32.dll");
@@ -65,16 +69,15 @@ namespace LoLUpdater
     "Pando Networks", "Media Booster", "uninst.exe");
 
         private const string AirMd5 = "179a1fcfcb54e3e87365e77c719a723f";
-
         private const string FlashMd5 = "9700dbdebffe429e1715727a9f76317b";
         private const string CgMd5 = "ae87223e882670029450b3f86e8e9300";
         private const string CgGlMd5 = "68dbb8778903f5cf0a80c00ffbf494d2";
         private const string CgD3D9Md5 = "9981b512f27b566d811b53590f6ee526";
 
         private static readonly string TbbMd5 = IsMultiCore
-                ? (Isx64 && (IsAtLeastWinNt6 || IsUnix) && IsAvx2
+                ? (IsAvx2
                     ? "db0767dc94a2d1a757c783f6c7994301"
-                    : (Isx64 && (IsAtLeastWinNt6 || IsUnix) && IsProcessorFeaturePresent(17)
+                    : (HasAvx
                         ? "2f178dadd7202b6a13a3409543a6fa86"
                         : (HasSse2 ? "1639aa390bfd02962c5c437d201045cc" : HasSse ? "3bf888228b83c4407d2eea6a5ab532bd" : "44dde7926b6dfef4686f2ddd19c04e2d")))
                 : (HasSse2 ? "82ed3be353217c61ff13a01bc85f1395" : (HasSse ? "eacd37174f1a4316345f985dc456a961" : "b389f80072bc877a6ef5ff33ade88a64"));
@@ -103,7 +106,7 @@ namespace LoLUpdater
             if (_userInput == 2)
             {
                 Console.WriteLine("Uninstalling, please wait...");
-                if (Directory.Exists("RADS"))
+                if (IsNotGarena)
                 {
                     BakCopy("Cg.dll", "solutions", "lol_game_client_sln", SlnFolder, string.Empty, IsInstalling);
                     BakCopy("CgD3D9.dll", "solutions", "lol_game_client_sln", SlnFolder, string.Empty, IsInstalling);
@@ -142,7 +145,7 @@ namespace LoLUpdater
                 {
                     Directory.CreateDirectory("Backup");
                 }
-                if (Directory.Exists("RADS"))
+                if (IsNotGarena)
                 {
                     BakCopy("solutions", "lol_game_client_sln", "cg.dll", SlnFolder, string.Empty, IsInstalling);
                     BakCopy("solutions", "lol_game_client_sln", "cgD3D9.dll", SlnFolder, string.Empty, IsInstalling);
@@ -201,7 +204,7 @@ namespace LoLUpdater
                   Path.Combine("Air", "Adobe Air", "Versions", "1.0", "Adobe AIR.dll"), AirMd5);
             Md5Check("projects", "lol_air_client", AirFolder,
     Path.Combine("Air", "Adobe Air", "Versions", "1.0", "Resources", "NPSWF32.dll"), FlashMd5);
-            if (Directory.Exists("RADS"))
+            if (IsNotGarena)
             {
                 Md5Check("solutions", "lol_game_client_sln", SlnFolder,
         "cg.dll", CgMd5);
@@ -215,8 +218,8 @@ namespace LoLUpdater
             else
             {
                 Md5Check(string.Empty, string.Empty, string.Empty, Path.Combine("Game", "cg.dll"), CgMd5);
-                Md5Check(string.Empty, string.Empty, string.Empty, Path.Combine("Game", "cgGL.dll"), CgMd5);
-                Md5Check(string.Empty, string.Empty, string.Empty, Path.Combine("Game", "cgD3D9.dll"), CgMd5);
+                Md5Check(string.Empty, string.Empty, string.Empty, Path.Combine("Game", "cgGL.dll"), CgGlMd5);
+                Md5Check(string.Empty, string.Empty, string.Empty, Path.Combine("Game", "cgD3D9.dll"), CgD3D9Md5);
                 Md5Check(string.Empty, string.Empty, string.Empty, Path.Combine("Game", "tbb.dll"), TbbMd5);
             }
 
@@ -230,27 +233,26 @@ namespace LoLUpdater
             Environment.Exit(0);
         }
 
-        // Todo: add MD5 checksums for original files.
         private static void Md5Check(string path, string path1, string ver, string file, string md5)
         {
-            if (Directory.Exists("RADS"))
+            if (IsNotGarena)
             {
                 Console.WriteLine(
-                    Md5(DirPath(path, path1, ver, file), md5) ? "{0} Is the latest patched version" : "{0} Is an old patched version or the original",
+                    !Md5Compare(DirPath(path, path1, ver, file), md5) ? "{0} Is an old patched version or the original" : "{0} Is the latest patched version",
                     file);
             }
             else
             {
                 Console.WriteLine(
-                    Md5(file, md5) ? "{0} Is the latest patched version" : "{0} Is an old patched version or the original",
+                    !Md5Compare(file, md5) ? "{0} Is an old patched version or the original" : "{0} Is the latest patched version",
                     file);
             }
         }
 
         static private int DisplayMenu()
         {
-            Console.WriteLine("LoLUpdater");
-            Console.WriteLine();
+            Console.WriteLine("Menu");
+            Console.WriteLine("Press 1 or 2");
             Console.WriteLine("1. Install");
             Console.WriteLine("2. Uninstall");
             return Convert.ToInt32(Console.ReadLine());
@@ -260,7 +262,7 @@ namespace LoLUpdater
         {
             using (WebClient webClient = new WebClient())
             {
-                if (Directory.Exists("RADS"))
+                if (IsNotGarena)
                 {
                     FileFix(path, path1, file, version, true);
                     if (!File.Exists(DirPath(path, path1, version, file)))
@@ -271,7 +273,7 @@ namespace LoLUpdater
                     }
                     else
                     {
-                        if (Md5(DirPath(path, path1, version, file), md5))
+                        if (Md5Compare(DirPath(path, path1, version, file), md5))
                         {
                             webClient.DownloadFile(
                                 uri,
@@ -289,14 +291,14 @@ namespace LoLUpdater
                     }
                     else
                     {
-                        if (Md5(file, md5))
+                        if (Md5Compare(file, md5))
                         {
                             webClient.DownloadFile(uri, file);
                         }
                     }
                     FileFix(file, String.Empty, String.Empty, String.Empty, false);
                 }
-                // Check already exists for the latest cg, but a double check might not hurt.
+
                 if (!String.IsNullOrEmpty(_cgBinPath) && new Version(
                     FileVersionInfo.GetVersionInfo(Path.Combine(_cgBinPath, "cg.dll")).FileVersion) >= LatestCg) return;
                 webClient.DownloadFile(
@@ -383,7 +385,7 @@ namespace LoLUpdater
 
         private static void Copy(string from, string file, string path, string path1, string ver, string to)
         {
-            if (Directory.Exists("RADS"))
+            if (IsNotGarena)
             {
                 FileFix(path, path1, file, SlnFolder, true);
                 if (!File.Exists(Path.Combine(
@@ -410,15 +412,15 @@ namespace LoLUpdater
             if (mode)
             {
                 if (File.ReadAllText(Path.Combine(path, file))
-                    .Contains(Tweak)) return;
+                    .Contains(CfgTweak)) return;
                 File.AppendAllText(Path.Combine(path, file),
-                    String.Format("{0}{1}", Environment.NewLine, Tweak));
+                    String.Format("{0}{1}", Environment.NewLine, CfgTweak));
             }
             else
             {
                 var oldLines = File.ReadAllLines(Path.Combine(path, file));
-                if (!oldLines.Contains(Tweak)) return;
-                var newLines = oldLines.Select(line => new { Line = line, Words = line.Split(' ') }).Where(lineInfo => !lineInfo.Words.Contains(Tweak)).Select(lineInfo => lineInfo.Line);
+                if (!oldLines.Contains(CfgTweak)) return;
+                var newLines = oldLines.Select(line => new { Line = line, Words = line.Split(' ') }).Where(lineInfo => !lineInfo.Words.Contains(CfgTweak)).Select(lineInfo => lineInfo.Line);
                 File.WriteAllLines(Path.Combine(path, file), newLines);
             }
             FileFix(Path.Combine(path, file), String.Empty, String.Empty, String.Empty, false);
@@ -426,7 +428,7 @@ namespace LoLUpdater
 
         private static string Version(string path, string path1)
         {
-            return Directory.Exists("RADS") ? Path.GetFileName(Directory.GetDirectories(Path.Combine("RADS", path, path1, "releases")).Max()) : String.Empty;
+            return IsNotGarena ? Path.GetFileName(Directory.GetDirectories(Path.Combine("RADS", path, path1, "releases")).Max()) : String.Empty;
         }
 
         [DllImport("kernel32.dll")]
@@ -444,7 +446,7 @@ namespace LoLUpdater
             return result;
         }
 
-        private static bool Md5(string file, string md5)
+        private static bool Md5Compare(string file, string md5)
         {
             using (FileStream fs = new FileStream(file, FileMode.Open, FileAccess.Read, FileShare.Read))
             {
