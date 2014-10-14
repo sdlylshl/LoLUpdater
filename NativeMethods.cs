e5bce7e1f2296af29ca73733490ffa1a3058172d
Ilja Korsun <ilja.korsun@gmail.com>
2014-10-14 03:23:21 +0200

    Update NativeMethods.cs

--- NativeMethods.cs
+++ NativeMethods.cs
@@ -1,22 +1,22 @@
 ﻿using System;
 using System.Runtime.InteropServices;
-
+private const string s_kernel = "kernel32.dll";
 namespace LoLUpdater
 {
     internal class NativeMethods
     {
-        [DllImport("kernel32.dll", CharSet = CharSet.Unicode)]
+        [DllImport(s_kernel, CharSet = CharSet.Unicode)]
         [return: MarshalAs(UnmanagedType.Bool)]
         public static extern void DeleteFile(string file);
 
-        [DllImport("kernel32", CharSet = CharSet.Ansi, BestFitMapping = false)]
+        [DllImport(s_kernel, CharSet = CharSet.Ansi, BestFitMapping = false)]
         public static extern IntPtr GetProcAddress(IntPtr hModule, string proc);
 
-        [DllImport("kernel32.dll", CharSet = CharSet.Unicode)]
+        [DllImport(s_kernel, CharSet = CharSet.Unicode)]
         [return: MarshalAs(UnmanagedType.Bool)]
         public static extern bool IsProcessorFeaturePresent(uint feature);
 
-        [DllImport("kernel32", CharSet = CharSet.Unicode)]
+        [DllImport(s_kernel, CharSet = CharSet.Unicode)]
         public static extern IntPtr LoadLibrary(string file);
     }
 }
