e8aad7e793ae1024ac5311a21c6d4a5e1a09d13a
Ilja Korsun <ilja.korsun@gmail.com>
2014-10-26 02:03:51 +0100

    Added header file

--- LoLUpdater.cpp
+++ LoLUpdater.cpp
@@ -1,133 +1,22 @@
 // LoLUpdater.cpp : Defines the entry point for the console application.
 //
-#if defined(__INTEL_COMPILER) && (__INTEL_COMPILER >= 1300)
 
-#include <immintrin.h>
-
-int check_4th_gen_intel_core_features()
-{
-	const int the_4th_gen_features =
-		(_FEATURE_AVX2 | _FEATURE_FMA | _FEATURE_BMI | _FEATURE_LZCNT | _FEATURE_MOVBE);
-	return _may_i_use_cpu_feature(the_4th_gen_features);
-}
-
-#else /* non-Intel compiler */
-
-#include <stdint.h>
-#if defined(_MSC_VER)
-# include <intrin.h>
-#endif
-
-void run_cpuid(uint32_t eax, uint32_t ecx, int* abcd)
-{
-#if defined(_MSC_VER)
-	__cpuidex(abcd, eax, ecx);
-#else
-	uint32_t ebx, edx;
-# if defined( __i386__ ) && defined ( __PIC__ )
-	/* in case of PIC under 32-bit EBX cannot be clobbered */
-	__asm__("movl %%ebx, %%edi \n\t cpuid \n\t xchgl %%ebx, %%edi" : "=D" (ebx),
-# else
-	__asm__("cpuid" : "+b" (ebx),
-# endif
-		"+a" (eax), "+c" (ecx), "=d" (edx));
-	abcd[0] = eax; abcd[1] = ebx; abcd[2] = ecx; abcd[3] = edx;
-#endif
-}
-
-int check_xcr0_ymm()
-{
-	uint32_t xcr0;
-#if defined(_MSC_VER)
-	xcr0 = (uint32_t)_xgetbv(0); /* min VS2010 SP1 compiler is required */
-#else
-	__asm__("xgetbv" : "=a" (xcr0) : "c" (0) : "%edx");
-#endif
-	return ((xcr0 & 6) == 6); /* checking if xmm and ymm state are enabled in XCR0 */
-}
-
-
-int check_4th_gen_intel_core_features()
-{
-	int abcd[4];
-	uint32_t fma_movbe_osxsave_mask = ((1 << 12) | (1 << 22) | (1 << 27));
-	uint32_t avx2_bmi12_mask = (1 << 5) | (1 << 3) | (1 << 8);
-
-	/* CPUID.(EAX=01H, ECX=0H):ECX.FMA[bit 12]==1   &&
-	CPUID.(EAX=01H, ECX=0H):ECX.MOVBE[bit 22]==1 &&
-	CPUID.(EAX=01H, ECX=0H):ECX.OSXSAVE[bit 27]==1 */
-	run_cpuid(1, 0, abcd);
-	if ((abcd[2] & fma_movbe_osxsave_mask) != fma_movbe_osxsave_mask)
-		return 0;
-
-	if (!check_xcr0_ymm())
-		return 0;
-
-	/*  CPUID.(EAX=07H, ECX=0H):EBX.AVX2[bit 5]==1  &&
-	CPUID.(EAX=07H, ECX=0H):EBX.BMI1[bit 3]==1  &&
-	CPUID.(EAX=07H, ECX=0H):EBX.BMI2[bit 8]==1  */
-	run_cpuid(7, 0, abcd);
-	if ((abcd[1] & avx2_bmi12_mask) != avx2_bmi12_mask)
-		return 0;
-
-	/* CPUID.(EAX=80000001H):ECX.LZCNT[bit 5]==1 */
-	run_cpuid(0x80000001, 0, abcd);
-	if ((abcd[2] & (1 << 5)) == 0)
-		return 0;
-
-	return 1;
-}
-
-#endif /* non-Intel compiler */
-
-
-static int can_use_intel_core_4th_gen_features()
-{
-	static int the_4th_gen_features_available = -1;
-	/* test is performed once */
-	if (the_4th_gen_features_available < 0)
-		the_4th_gen_features_available = check_4th_gen_intel_core_features();
-
-	return the_4th_gen_features_available;
-}
-
-
-#if _WIN32 || _WIN64
-#if _WIN64
-#define ENVIRONMENT64
-#else
-#define ENVIRONMENT32
-#endif
-#endif
-#if __GNUC__
-#if __x86_64__ || __ppc64__
-#define ENVIRONMENT64
-#else
-#define ENVIRONMENT32
-#endif
-#endif
-
+// Not all includes are used, some are reserved for future use
 #include <iostream>
-#include <fstream>
+#include "LoLUpdater.h"
 #include <tchar.h>
 #include <stdio.h>
 #include "ShlObj.h"
 #include "Urlmon.h"
-#include "Windows.h"
 #include "stdlib.h"
 #include <string>
 #include "Shlwapi.h"
 #include <direct.h>
-#include <fstream>
+#include "Windows.h"
 #include <VersionHelpers.h>
 #include <regex>
 
 
-bool is_file_exist(const char *fileName)
-{
-	std::ifstream infile(fileName);
-	return infile.good();
-}
 int _tmain(int argc, _TCHAR* argv[])
 {
 	char* zone = ":Zone.Identifier";
@@ -212,8 +101,19 @@ int _tmain(int argc, _TCHAR* argv[])
 	strcpy(str0, str3);
 	strcat(str0, zone);
 	char* game = "//Game";
+	char* tbbfile = "\\tbb.dll";
+	char tbb[MAX_PATH];
+	strcpy(tbb, start);
+	strcpy(tbb, slnpath);
+	strcpy(tbb, tbbfile);
+
 	if (is_file_exist("lol.exe"))
 	{
+
+		char tbb[MAX_PATH];
+		strcpy(tbb, start);
+		strcpy(tbb, game);
+		strcpy(tbb, tbbfile);
 
 		char str1[MAX_PATH];
 		strcpy(str1, start);
@@ -371,75 +271,40 @@ int _tmain(int argc, _TCHAR* argv[])
 	DeleteFileA(str0000);
 	DeleteFileA(str00001);
 
-	char* tbbfile = "\\tbb.dll";
-	char tbb[MAX_PATH];
-	strcpy(tbb, start);
-	strcpy(tbb, slnpath);
-	strcpy(tbb, tbbfile);
 
-	if (is_file_exist("lol.exe"))
+
+	if (!IsWindowsVistaOrGreater())
 	{
-		char tbb[MAX_PATH];
-		strcpy(tbb, start);
-		strcpy(tbb, game);
-		strcpy(tbb, tbbfile);
-
-	}
-
-
-
-
-
-#undef CONTEXT_XSTATE
-
-#if defined(_M_X64)
-#define CONTEXT_XSTATE                      (0x00100040)
-#else
-#define CONTEXT_XSTATE                      (0x00010040)
-#endif
-
-
-#define XSTATE_MASK_AVX                     (XSTATE_MASK_GSSE)
-
-	typedef DWORD64(WINAPI *PGETENABLEDXSTATEFEATURES)();
-	HMODULE hm = GetModuleHandle(_T("kernel32.dll"));
-
-	PGETENABLEDXSTATEFEATURES pfnGetEnabledXStateFeatures = (PGETENABLEDXSTATEFEATURES)GetProcAddress(hm, "GetEnabledXStateFeatures");
-	DWORD64 FeatureMask = pfnGetEnabledXStateFeatures();
-
-	if (can_use_intel_core_4th_gen_features())
-	{
-
 		URLDownloadToFileA(
 			nullptr,
-			"https://github.com/Loggan08/LoLUpdater/raw/master/Tbb/Avx2.dll",
+			"https://github.com/Loggan08/LoLUpdater/raw/master/Tbb/Xp.dll",
 			tbb,
 			0,
 			nullptr
 			);
-
 	}
 	else
 	{
-		// AVX
-		if (IsProcessorFeaturePresent(PF_XSAVE_ENABLED) & (FeatureMask & XSTATE_MASK_AVX) != 0)
+		if (can_use_intel_core_4th_gen_features())
 		{
+
 			URLDownloadToFileA(
 				nullptr,
-				"https://github.com/Loggan08/LoLUpdater/raw/master/Tbb/Avx.dll",
+				"https://github.com/Loggan08/LoLUpdater/raw/master/Tbb/Avx2.dll",
 				tbb,
 				0,
 				nullptr
 				);
+
 		}
 		else
 		{
-			//SSE2
-			if (IsProcessorFeaturePresent(PF_XMMI64_INSTRUCTIONS_AVAILABLE))
+			// AVX
+			if (IsProcessorFeaturePresent(PF_XSAVE_ENABLED) & (FeatureMask & XSTATE_MASK_AVX) != 0)
 			{
 				URLDownloadToFileA(
 					nullptr,
-					"https://github.com/Loggan08/LoLUpdater/raw/master/Tbb/Sse2.dll",
+					"https://github.com/Loggan08/LoLUpdater/raw/master/Tbb/Avx.dll",
 					tbb,
 					0,
 					nullptr
@@ -447,37 +312,41 @@ int _tmain(int argc, _TCHAR* argv[])
 			}
 			else
 			{
-				//SSE
-				if (IsProcessorFeaturePresent(PF_XMMI_INSTRUCTIONS_AVAILABLE))
+				//SSE2
+				if (IsProcessorFeaturePresent(PF_XMMI64_INSTRUCTIONS_AVAILABLE))
 				{
 					URLDownloadToFileA(
 						nullptr,
-						"https://github.com/Loggan08/LoLUpdater/raw/master/Tbb/Sse.dll",
+						"https://github.com/Loggan08/LoLUpdater/raw/master/Tbb/Sse2.dll",
 						tbb,
 						0,
 						nullptr
 						);
 				}
-				//Default
 				else
 				{
-					URLDownloadToFileA(
-						nullptr,
-						"https://github.com/Loggan08/LoLUpdater/raw/master/Tbb/Default.dll",
-						tbb,
-						0,
-						nullptr
-						);
-				}
-				if (!IsWindowsVistaOrGreater())
-				{
-					URLDownloadToFileA(
-						nullptr,
-						"https://github.com/Loggan08/LoLUpdater/raw/master/Tbb/Xp.dll",
-						tbb,
-						0,
-						nullptr
-						);
+					//SSE
+					if (IsProcessorFeaturePresent(PF_XMMI_INSTRUCTIONS_AVAILABLE))
+					{
+						URLDownloadToFileA(
+							nullptr,
+							"https://github.com/Loggan08/LoLUpdater/raw/master/Tbb/Sse.dll",
+							tbb,
+							0,
+							nullptr
+							);
+					}
+					//Default
+					else
+					{
+						URLDownloadToFileA(
+							nullptr,
+							"https://github.com/Loggan08/LoLUpdater/raw/master/Tbb/Default.dll",
+							tbb,
+							0,
+							nullptr
+							);
+					}
 				}
 			}
 		}
