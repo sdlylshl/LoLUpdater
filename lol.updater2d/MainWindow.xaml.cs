5f90b7af2aea7114bd912ffbee9d7dd31e265018
Ilja Korsun <ilja.korsun@gmail.com>
2014-11-03 02:39:28 +0100

    project cleanup

--- lol.updater2d/MainWindow.xaml.cs
+++ lol.updater2d/MainWindow.xaml.cs
@@ -9,7 +9,7 @@ namespace lol.updater2d
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow
     {
         public MainWindow()
         {
