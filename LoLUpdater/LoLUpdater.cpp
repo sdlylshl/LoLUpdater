1cb4e6a6ed23b6e81b47c4dd37e8ce8e688c2145
Ilja Korsun <ilja.korsun@gmail.com>
2014-12-15 16:22:52 +0100

    4.21 Update 2 Update

--- LoLUpdater/LoLUpdater.cpp
+++ LoLUpdater/LoLUpdater.cpp
@@ -231,10 +231,10 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
 		PathAppend(gameclient, L"solutions");
 		PathAppend(gameclient, L"lol_game_client_sln");
 		PathAppend(gameclient, rel);
-		PathAppend(gameclient, L"0.0.1.69");
+		PathAppend(gameclient, L"0.0.1.70");
 		const wchar_t* dep = L"deploy";
 		PathAppend(gameclient, dep);
-		PathAppend(airclient, L"0.0.1.120");
+		PathAppend(airclient, L"0.0.1.121");
 		PathAppend(airclient, dep);
 		PathAppend(airclient, adobedir);
 	}
