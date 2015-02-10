6e6c9367628bcc58be48a5467982c1e3f1865c1e
Ilja Korsun <ilja.korsun@gmail.com>
2015-02-10 06:13:56 +0100

    update

--- LoLUpdater AutoUpdater/LoLUpdater.cpp
+++ LoLUpdater AutoUpdater/LoLUpdater.cpp
@@ -1,10 +1,7 @@
 #include <Windows.h>
-
-void downloadFile(std::wstring const& url, std::wstring const& file)
-{
-	if (URLDownloadToFile(nullptr, url.c_str(), file.c_str(), 0, nullptr) != S_OK)
-		throw std::runtime_error("failed to initialize download");
-}
+#include <string>
+#include <fstream>
+#include <sstream>
 
 class CLimitSingleInstance
 {
@@ -15,7 +12,7 @@ protected:
 public:
 	explicit CLimitSingleInstance(std::wstring const& strMutexName)
 	{
-		m_hMutex = CreateMutex(nullptr, 0, strMutexName.c_str());
+		m_hMutex = CreateMutexW(nullptr, 0, strMutexName.c_str());
 		if (m_hMutex == nullptr)
 			throw std::runtime_error("failed to create mutex");
 
@@ -39,15 +36,63 @@ public:
 	}
 };
 
-CLimitSingleInstance g_SingleInstanceObj(L"Global\\{101UPD473R4U70UPD473R-BYL0GG4N08@G17HUB-V3RYR4ND0M4NDR4R3MUCH}");
 
+CLimitSingleInstance g_SingleInstanceObj(L"Global\\{101UPD473R4U70UPD473R-BYL0GG4N08@G17HUB-V3RYR4ND0M4NDR4R3MUCH}");
+bool noupdate = false;
+bool update = false;
+
+void downloadFile(std::wstring const& url, std::wstring const& file)
+{
+	if (URLDownloadToFileW(nullptr, url.c_str(), file.c_str(), 0, nullptr) != S_OK)
+		throw std::runtime_error("failed to initialize download");
+}
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+	HDC hdc;
+	PAINTSTRUCT ps;
+	RECT start = { 2, 0, 0, 0 };
+	RECT end = { 2, 20, 0, 0 };
+	switch (msg)
+	{
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	case WM_PAINT:
+		hdc = BeginPaint(hwnd, &ps);
+		if (hdc == nullptr)
+			throw std::runtime_error("Nothing to render in");
+
+		if (DrawTextW(hdc, L"Checking for Updates!", -1, &start, DT_SINGLELINE | DT_NOCLIP) == NULL)
+			throw std::runtime_error("failed to draw text");
+
+		if (update)
+		{
+			if (DrawTextW(hdc, L"Update Downloaded!", -1, &end, DT_SINGLELINE | DT_NOCLIP) == NULL)
+				throw std::runtime_error("failed to draw text");
+		}
+		if (noupdate)
+		{
+			if (DrawTextW(hdc, L"No Update Available", -1, &end, DT_SINGLELINE | DT_NOCLIP) == NULL)
+				throw std::runtime_error("failed to draw text");
+		}
+		EndPaint(hwnd, &ps);
+		break;
+	default:
+		return DefWindowProc(hwnd, msg, wParam, lParam);
+	}
+	return 0;
+}
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE,
 	LPSTR, int nCmdShow)
 {
 	const std::wstring latestversion = L"latestversion.txt";
 	downloadFile(L"http://lol.jdhpro.com/latestversion.txt", latestversion.c_str());
-	std::ifstream t("file.txt");
-	std::stringstream buffer;
+	std::wifstream t(L"file.txt");
+	std::wstringstream buffer;
 	buffer << t.rdbuf();
+
+	finished = true;
+	finished = false;
 }
