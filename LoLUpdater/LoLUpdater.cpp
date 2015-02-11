d33b2227107d066f979531ade0341c937a54d991
Ilja Korsun <ilja.korsun@gmail.com>
2015-02-11 21:25:44 +0100

    pretty up aboutbox

--- LoLUpdater/LoLUpdater.cpp
+++ LoLUpdater/LoLUpdater.cpp
@@ -445,9 +445,10 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 		hwndButton2 = CreateWindow(L"button", L"Uninstall", WS_VISIBLE | WS_CHILD | BS_PUSHBUTTON, 120, 10, 100, 50, hwnd, (HMENU)200, nullptr, nullptr);
 		OldButtonProc2 = reinterpret_cast<WNDPROC>(SetWindowLong(hwndButton2, GWL_WNDPROC, reinterpret_cast<LONG>(ButtonProc2)));
 		
+                             
 		MDIClientCreateStruct.hWindowMenu = nullptr;
 		MDIClientCreateStruct.idFirstChild = IDM_FIRSTCHILD;
-		hwnd2 = CreateWindow(L"MDICLIENT", nullptr, WS_OVERLAPPEDWINDOW | WS_CLIPCHILDREN, CW_USEDEFAULT, CW_USEDEFAULT, 250, 130,hwnd,nullptr,nullptr,(void*)&MDIClientCreateStruct);
+		hwnd2 = CreateWindowEx(WS_EX_TOOLWINDOW, L"MDICLIENT", L"About LoLUpdater", WS_OVERLAPPEDWINDOW | WS_CLIPCHILDREN, CW_USEDEFAULT, CW_USEDEFAULT, 250, 130, hwnd, nullptr, nullptr, (void*)&MDIClientCreateStruct);
 
 		if (hwnd2 == nullptr)
 		{
